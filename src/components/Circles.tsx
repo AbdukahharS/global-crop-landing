f7b8293fb4eef007ab62cbab2b673e759627504c
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2025-10-19 19:24:47 +0500

    Refactor code structure for improved readability and maintainability

--- src/components/Circles.tsx
+++ src/components/Circles.tsx
@@ -4,6 +4,7 @@ const Circles = () => {
         <div className='shadow-circle top-[189px] left-[194px]'></div>
         <div className='shadow-circle top-[95vh] left-[267px]'></div>
         <div className='shadow-circle top-[92vh] right-[56px]'></div>
+        <div className='shadow-circle top-[120vh] right-[93px]'></div>
     </>
   )
 }
